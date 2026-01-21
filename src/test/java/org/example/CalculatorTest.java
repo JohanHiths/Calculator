05a9351ef5cda551c988f9c3b711a72c10ac4a0d
JohanH <johan.hietala@iths.se>
2026-01-21 22:37:07 +0100

    updated release.yml and pom.xml

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,9 +1,11 @@
 package org.example;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 public class CalculatorTest {
 
