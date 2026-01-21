d4db3d73ef3e6353e6da0d0d372ea7756b227de1
JohanH <johan.hietala@iths.se>
2026-01-21 11:42:23 +0100

    Made more methods, added yml files

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -29,4 +29,12 @@ public class Calculator {
         return a - b;
     }
 
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 
 public class Main {
     static void main() {
+    Calculator calc = new Calculator();
 
+
+        System.out.println(calc.add(2, 3));
     }
 }
