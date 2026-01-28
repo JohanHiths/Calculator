4b95de5acb12998c54d8f8c5dc70db8e3b9872ba
JohanH <johan.hietala@iths.se>
2026-01-28 08:45:39 +0100

    Börjat med toDoList, toDoListTest

---
+++ src/test/java/org/example/toDoListTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class toDoListTest {
+
+    @Test
+    void toDoListEmpty(){
+        toDoList list = new toDoList();
+
+
+    }
+}
